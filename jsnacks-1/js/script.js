c4616fa0145ba587a8a649576c772e52cdbe2077
DavideTessari <davtex@gmail.com>
2024-03-12 15:33:17 +0100

    complete jsnack-1

--- jsnacks-1/js/script.js
+++ jsnacks-1/js/script.js
@@ -1,2 +1,18 @@
 // L'utente inserisce un numero nel prompt, se è pari stampa il numero,
 // se è dispari stampa il numero successivo
+
+// Inizializza un array vuoto per i Numeri
+const numbers = [];
+
+// Chiedi all'utente un numero e stampalo se è pari
+const userNumber = parseInt(prompt("Inserisci un numero:"));
+
+// Verifica se il numero è pari o dispari
+if (userNumber % 2 === 0) {
+    numbers.push(userNumber);
+} else {
+    numbers.push(userNumber + 1);
+    }
+
+alert(numbers);
+
